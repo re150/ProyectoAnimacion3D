fe0cb37a943435429a2d346e67a9cc1129905fb0
re150 <a21110175@live.ceti.mx>
2023-11-30 14:44:15 -0600

    ya se puedes dibujar las figuras 3D

--- Animacion3D/src/Main.java
+++ Animacion3D/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             Pixel g = new Pixel();
-            new Thread(g).start();
+
         });
     }
 }
--- Animacion3D/src/Pixel.java
+++ Animacion3D/src/Pixel.java
@@ -3,25 +3,45 @@ import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Pixel extends JPanel implements Runnable {
+// para extraer las cordenadas
+
+public class Pixel extends JPanel {
     private BufferedImage buffer;
     private BufferedImage fondo;
     private Graphics fondoG;
-    private double vertex[][][][] = {{{{-1, -1, -1}, {1, -1, -1}}, {{-1, 1, -1},{1, 1, -1}}}, {{{-1, -1, 1}, {1, -1, 1}}, {{-1, 1, 1}, {1, 1, 1}}}};
+    private double vertex[][][][] = {{{{-1, -1, -1}, {1, -1, -1}}, {{-1, 1, -1}, {1, 1, -1}}}, {{{-1, -1, 1}, {1, -1, 1}}, {{-1, 1, 1}, {1, 1, 1}}}};
+    private
+    // Define vertices for a pyramid
+    double[][][] vertex1 = {
+            {{-1, -1, -1}, {1, -1, -1}, {0, -1, 1}}, // Base
+            {{-1, -1, -1}, {1, -1, -1}, {0, 1, 0}},   // Side 1
+            {{1, -1, -1}, {0, -1, 1}, {0, 1, 0}},     // Side 2
+            {{0, -1, 1}, {-1, -1, -1}, {0, 1, 0}}     // Side 3
+    };
+
     // rotations in radians
-    private double xyR = 0, xzR = 0, yzR =0;
+    private double xyR = 0, xzR = 0, yzR = 0;
 
     // view attributes
-    private int posicion = 0, scala = 0 ;
-    private int viewX = 5 , viewY = 20 , viewZ = -20 , tX =0, tY =0;
+    private int posicion = 0;
+    private  double scala = 0.10;
+    private int viewX = 5, viewY = 20, viewZ = -20, tX = 0, tY = 0;
     private double t = 0;
-    private double x , y, z, xf, yf, zf,sideCube  = 0;
-    private  double[][] matrix = new double[4][241];
+    private double x, y, z, xf, yf, zf, sideCube = 0;
+    private double[][] matrix = new double[4][241];
     private double[][] matrix1 = new double[4][241];
     private double tethax = 0;
     private double tethay = 0;
     private double tethaz = 0;
+    private boolean estado1 = true;
+    List<double[]> vertices;
+
     public Pixel() {
         init();
         repaint();
@@ -30,57 +50,71 @@ public class Pixel extends JPanel implements Runnable {
         setDoubleBuffered(true);
 
         addKeyListener(new KeyAdapter() {
-             @Override
-                 public void keyPressed(KeyEvent e) {
-                    int key = e.getKeyCode();
-                    if (key == KeyEvent.VK_LEFT) {
-                        viewX -=5;
-                        t -= 0.005;
-                        // Ajustar rotación izquierda
-                        if(xyR  > 0) xyR -= 0.001;
-                        System.out.println("xyR = " + xyR);
-                    }
-                    if (key == KeyEvent.VK_RIGHT) {
-                        viewX += 5;
-                        t += 0.005;
-                        // Ajustar rotación derecha
-                        if(xyR < 0.005)xyR += 0.001;
-                    }
-                    if (key == KeyEvent.VK_UP) {
-                        viewY -= 5;
-                        if(xzR < 0.005) xzR +=  0.001;
-                        tethax += 1;
-                        sideCube +=5;
-                    }
-                    if (key == KeyEvent.VK_DOWN) {
-                        viewY +=5;
-                        if(xzR > 0) xzR -=  0.001;
-                         tethax -= 1;
-                         sideCube -=5;
-                    }
-                    if (key == KeyEvent.VK_Z ){
-                        viewZ += 5;
-                    }
-                    if (key == KeyEvent.VK_X ){
-                        viewZ -= 5;
-                    }
-                    if (key == KeyEvent.VK_W ){
-                         tX += 5;
-                     }
-                    if (key == KeyEvent.VK_S ){
-                        tX -= 5;
-                     }
-                    if (key == KeyEvent.VK_D ){
-                         if(scala < 100) scala += 10;
-                     }
-                    if(key == KeyEvent.VK_A){
-                         if(scala > 10)  scala -= 10;
-                     }
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int key = e.getKeyCode();
+                if (key == KeyEvent.VK_LEFT) {
+                    viewX -= 5;
+                    t -= 0.005;
+                    // Ajustar rotación izquierda
+                    if (xyR > 0) xyR -= 0.001;
+                    System.out.println("xyR = " + xyR);
                 }
+                if (key == KeyEvent.VK_RIGHT) {
+                    viewX += 5;
+                    t += 0.005;
+                    // Ajustar rotación derecha
+                    if (xyR < 0.005) xyR += 0.001;
+                }
+                if (key == KeyEvent.VK_UP) {
+                    viewY -= 5;
+                    if (xzR < 0.05) xzR += 0.01;
+                    tethax += 1;
+                    sideCube += 5;
+                    posicion += 5;
+                }
+                if (key == KeyEvent.VK_DOWN) {
+                    viewY += 5;
+                    if (xzR > 0) xzR -= 0.01;
+                    tethax -= 1;
+                    sideCube -= 5;
+                    posicion -= 5;
+                }
+                if (key == KeyEvent.VK_Z) {
+                    viewZ += 5;
+                }
+                if (key == KeyEvent.VK_X) {
+                    viewZ -= 10;
+                    estado1 = !estado1;
+                }
+                if (key == KeyEvent.VK_W) {
+                    tX += 10;
+                }
+                if (key == KeyEvent.VK_S) {
+                    tX -= 10;
+
+                }
+                if (key == KeyEvent.VK_D) {
+                  if(scala < 0.20)  scala += 0.05;
+                }
+                if (key == KeyEvent.VK_A) {
+                    if(scala > 0.10)  scala -= 0.05;
+                }
+            }
         });
+        cordenadasObj();
     }
 
-    public void init () {
+    public void cordenadasObj (){
+        String filePath = "D:\\M6\\Gráficas_por_computadora_2D_y_3D\\p3\\Figuras\\Airplane_v1_L1.123c4a6fedec-1680-4a36-a228-b0d440a4f280\\11803_Airplane_v1_l1.obj";
+         vertices = extractVerticesFromOBJ(filePath);
+        // Imprime las coordenadas de los vértices
+        for (double[] vertex : vertices) {
+            System.out.println("Vertex: " + vertex[0] + ", " + vertex[1] + ", " + vertex[2]);
+        }
+    }
+
+    public void init() {
         setSize(700, 700);
         setVisible(true);
 
@@ -89,7 +123,7 @@ public class Pixel extends JPanel implements Runnable {
         fondoG = fondo.getGraphics();
         fondoG.setColor(Color.black);
         fondoG.fillRect(0, 0, getWidth(), getWidth());
-        matrix1 = curve(0,0,0,10);
+        matrix1 = curve(0, 0, 0, 10);
 
         JFrame frame = new JFrame("Pixel");
         frame.setResizable(false);
@@ -101,38 +135,49 @@ public class Pixel extends JPanel implements Runnable {
     }
 
     @Override
-    public void update (Graphics g){
+    public void update(Graphics g) {
         g.drawImage(fondo, 0, 0, this);
     }
 
     @Override
-    public void paint (Graphics g){
+    public void paint(Graphics g) {
         update(g);
         clear();
         try {
             // practica
-           tiposDeProyeccion(500, 400, 200, 50, 600, 0, Color.white);
-            tiposDeProyeccion(200, 100, 5, 500, 400, 0, Color.blue);
-            tiposDeProyeccion(600, 600, 5, 400, 800, 8, Color.RED);
-            drawCuboR(200,50,Color.red);
-            drawCurve(100);
+         // tiposDeProyeccion(500, 400, 200, 50, 600, 0, Color.white);
+         /*   tiposDeProyeccion(200, 100, 5, 500, 400, 0, Color.blue);*/
+            // tiposDeProyeccion(600, 600, 5, 400, 800, 8, Color.RED);
 
-        }catch (Exception e){}
+           //drawCuboR1(201,50,Color.green);
+
+            // drawCurve(100);
+            //   if(estado1) drawPyramidR(350,  Color.blue);
+
+           /*for (int i = 0; i < vertices.size() - 1; i++) {
+                double[] puntoActual = vertices.get(i);
+                double[] puntoSiguiente = vertices.get(i + 1);
+
+                drawDDL(puntoActual[0] * 0.09, puntoActual[1] * 0.09 , puntoActual[2] * 0.09 , puntoSiguiente[0] * 0.09 , puntoSiguiente[1]*  0.09, puntoSiguiente[2]*  0.09, Color.RED);
+            }*/
+            if(estado1)   drawFigura(300,Color.red);
+        } catch (Exception e) {
+        }
     }
 
-    public void clear () {
+    public void clear() {
         fondoG.setColor(Color.black);
         fondoG.fillRect(0, 0, getWidth(), getHeight());
         repaint();
     }
 
-    public void putPixel ( int x, int y, Color c){
-       buffer.setRGB(0, 0, c.getRGB());
-       fondo.getGraphics().drawImage(buffer, x, y, this);
-       repaint();
+    public void putPixel(int x, int y, Color c) {
+        buffer.setRGB(0, 0, c.getRGB());
+        fondo.getGraphics().drawImage(buffer, x, y, this);
+        repaint();
     }
 
-    public void algoritmoDDALine(int x0, int y0, int x1, int y1, Color c){
+    public void algoritmoDDALine(int x0, int y0, int x1, int y1, Color c) {
         float dx = x1 - x0;
         float dy = y1 - y0;
         float x = x0;
@@ -158,10 +203,11 @@ public class Pixel extends JPanel implements Runnable {
             putPixel(Math.round(x), Math.round(y), c);
         }
     }
-    public void drawDDL(double x1, double y1, double z1, double x2, double y2, double z2,Color c){
-          int xp = 200;
-          int yp = 200;
-          int zp = -130;
+
+    public void drawDDL(double x1, double y1, double z1, double x2, double y2, double z2, Color c) {
+        int xp = -50;
+        int yp = -50;
+        int zp = -130;
         double xUno = x1 - xp * (z1 / zp);
         double yUno = y1 - yp * (z1 / zp);
         double xDos = x2 - xp * (z2 / zp);
@@ -180,6 +226,7 @@ public class Pixel extends JPanel implements Runnable {
         yinc = dy / (double) steps;
         x = xUno;
         y = yUno;
+
         for (int k = 1; k <= steps; k++) {
             x = x + xinc;
             y = y + yinc;
@@ -188,123 +235,57 @@ public class Pixel extends JPanel implements Runnable {
 
     }
 
-    public void drawCuadrado ( int x1, int y1, int x2, int y2, Color c){
-        algoritmoDDALine(x1, y1, x2, y1, c);
-        algoritmoDDALine(x1, y1, x1, y2, c);
-        algoritmoDDALine(x2, y1, x2, y2, c);
-        algoritmoDDALine(x1, y2, x2, y2, c);
-    }
 
-    public void tiposDeProyeccion(int x1, int y1, int z1, int x2, int y2, int z2, Color c) {
-
-        int[][] cubeOnePoints;
-        int[][] cubeTwoPoints;
-
-        cubeOnePoints = cuboProyecion1(x1, y1, z1, x2, y2);
-        cubeTwoPoints = cuboProyecion2(x1, y1, x2, y2, z2);
-
-        drawCuadrado(cubeOnePoints[0][0], cubeOnePoints[0][1], cubeOnePoints[3][0], cubeOnePoints[3][1],Color.red);
-        drawCuadrado(cubeTwoPoints[0][0], cubeTwoPoints[0][1], cubeTwoPoints[1][0], cubeTwoPoints[1][1], c);
-
-        // se dibujan las uniones de los cuadrados
-        algoritmoDDALine(cubeOnePoints[0][0], cubeOnePoints[0][1], cubeTwoPoints[1][0], cubeTwoPoints[1][1], Color.green);
-        algoritmoDDALine(cubeOnePoints[1][0], cubeOnePoints[1][1], cubeTwoPoints[2][0], cubeTwoPoints[2][1], Color.green);
-        algoritmoDDALine(cubeOnePoints[2][0], cubeOnePoints[2][1], cubeTwoPoints[0][0], cubeTwoPoints[3][1], Color.green);
-        algoritmoDDALine(cubeOnePoints[3][0], cubeOnePoints[3][1], cubeTwoPoints[0][0], cubeTwoPoints[0][1], Color.green);
-
-
-    }
-
-    private int[][] cuboProyecion1(int x1, int y1, int z1, int x2, int y2) {
-        int[][] points = new int[4][2];
-
-        points[0] = convert(x1, y1, z1);
-        points[1] = convert(x1, y2, z1);
-        points[2] = convert(x2, y1, z1);
-        points[3] = convert(x2, y2, z1);
-
-        return points;
-    }
-
-    private int[][] cuboProyecion2(int x1, int y1, int x2, int y2, int z2) {
-        int[][] points = new int[4][2];
-
-        points[0] = convert(x2, y2, z2);
-        points[1] = convert(x1, y1, z2);
-        points[2] = convert(x1, y2, z2);
-        points[3] = convert(x2, y1, z2);
-
-        return points;
-    }
-
-    private int[] convert(int x, int y, int z) {
-        int xconvert, yconvert;
-        xconvert = viewX - ((viewZ * (x - viewX)) / (z - viewZ) + tX) ;
-        yconvert = viewY - ((viewZ * (y - viewY)) / (z - viewZ)  + tY) ;
-
-        return new int[]{xconvert, yconvert };
-    }
-
-    public  void drawCuboR(int posicionCubo , int scalaCubo , Color c){
-       scala =  scalaCubo;
-       posicion = posicionCubo;
+    public void drawCuboR1(int posicionCubo, Color c) {
+        posicion = posicionCubo;
         // rotate cube
         for (int x = 0; x < 2; x++) {
             for (int y = 0; y < 2; y++) {
                 for (int z = 0; z < 2; z++) {
-                    xyRotate(vertex[x][y][z], Math.sin(xyR), Math.cos(xyR));
-                    xzRotate(vertex[x][y][z], Math.sin(xzR), Math.cos(xzR));
-                    yzRotate(vertex[x][y][z], Math.sin(yzR), Math.cos(yzR));
+                    xyRotate1(vertex[x][y][z], Math.sin(xyR), Math.cos(xyR));
+                    xzRotate1(vertex[x][y][z], Math.sin(xzR), Math.cos(xzR));
+                    yzRotate1(vertex[x][y][z], Math.sin(yzR), Math.cos(yzR));
                 }
             }
         }
         //bordes
         for (int x = 0; x < 2; x++) {
             for (int y = 0; y < 2; y++) {
-                drawEdge(vertex[x][y][0][0], vertex[x][y][0][1], vertex[x][y][1][0], vertex[x][y][1][1], c);
-                drawEdge(vertex[x][0][y][0], vertex[x][0][y][1], vertex[x][1][y][0], vertex[x][1][y][1], c );
-                drawEdge(vertex[0][x][y][0], vertex[0][x][y][1], vertex[1][x][y][0], vertex[1][x][y][1],  c);
+                drawEdge1(vertex[x][y][0][0], vertex[x][y][0][1], vertex[x][y][1][0], vertex[x][y][1][1], c);
+                drawEdge1(vertex[x][0][y][0], vertex[x][0][y][1], vertex[x][1][y][0], vertex[x][1][y][1], c);
+                drawEdge1(vertex[0][x][y][0], vertex[0][x][y][1], vertex[1][x][y][0], vertex[1][x][y][1], c);
             }
         }
     }
-    final void drawEdge(double x1, double y1, double x2, double y2, Color c) {
+
+    final void drawEdge1(double x1, double y1, double x2, double y2, Color c) {
         algoritmoDDALine((int) (x1 * scala) + posicion, (int) (-y1 * scala) + posicion, (int) (x2 * scala) + posicion, (int) (-y2 * scala) + posicion, c);
     }
 
-    final void xyRotate(double p[], double sin, double cos) {
+    final void xyRotate1(double p[], double sin, double cos) {
         double temp;
         temp = cos * p[0] + sin * p[1];
         p[1] = -sin * p[0] + cos * p[1];
         p[0] = temp;
     }
 
-    final void xzRotate(double p[], double sin, double cos) {
+    final void xzRotate1(double p[], double sin, double cos) {
         double temp;
         temp = cos * p[0] + sin * p[2];
         p[2] = -sin * p[0] + cos * p[2];
         p[0] = temp;
     }
 
-    final void yzRotate(double p[], double sin, double cos) {
+    final void yzRotate1(double p[], double sin, double cos) {
         double temp;
         temp = cos * p[1] + sin * p[2];
         p[2] = -sin * p[1] + cos * p[2];
         p[1] = temp;
     }
 
-    @Override
-    public void run () {
-        try {
-            repaint();
-            clear();
-            animacion();
-            Thread.sleep(100);
-        } catch (InterruptedException ignored) {
-        }
-    }
 
-    public double[][] curve(double x, double y, double z,int sideCube) {
-        double[][] escala = { { sideCube, 0, 0, 0 }, { 0, sideCube, 0, 0 }, { 0, 0, sideCube, 0 }, { 0, 0, 0, 1 } };
+    public double[][] curve(double x, double y, double z, int sideCube) {
+        double[][] escala = {{sideCube, 0, 0, 0}, {0, sideCube, 0, 0}, {0, 0, sideCube, 0}, {0, 0, 0, 1}};
         matrix[0][0] = x;
         matrix[1][0] = y;
         matrix[2][0] = z;
@@ -334,6 +315,7 @@ public class Pixel extends JPanel implements Runnable {
         }
         return c;
     }
+
     static double[][] Rotarx(double[][] m, double angle) {
         double cos = Math.cos(Math.toRadians(angle));
         double sen = Math.sin(Math.toRadians(angle));
@@ -347,8 +329,8 @@ public class Pixel extends JPanel implements Runnable {
         return m;
     }
 
-    public  void drawCurve(int numCurba){
-            for (int i = 23; i < numCurba; i++) {
+    public void drawCurve(int numCurba) {
+        for (int i = 23; i < numCurba; i++) {
             x = matrix1[0][i];
             y = matrix1[1][i];
             z = matrix1[2][i];
@@ -356,8 +338,9 @@ public class Pixel extends JPanel implements Runnable {
             yf = matrix1[1][i + 1];
             zf = matrix1[2][i + 1];
             drawDDL(x + 350, y + 350, z, xf + 350, yf + 350, zf, Color.RED);
-         }
+        }
     }
+
     public double[][] translateOrigin(double[][] m) {
         double x = m[0][0];
         double y = m[1][0];
@@ -386,13 +369,14 @@ public class Pixel extends JPanel implements Runnable {
         }
         return m;
     }
+
     public void animacion() {
         while (true) {
 
             matrix1 = translateOrigin(matrix1);
-            matrix1 =Rotarx(matrix1, tethax);
-            matrix1 =Rotarx(matrix1, tethay);
-            matrix1 =Rotarx(matrix1, tethaz);
+            matrix1 = Rotarx(matrix1, tethax);
+            matrix1 = Rotarx(matrix1, tethay);
+            matrix1 = Rotarx(matrix1, tethaz);
             matrix1 = change(matrix1);
 
             repaint();
@@ -403,5 +387,54 @@ public class Pixel extends JPanel implements Runnable {
             }
         }
     }
+
+    private static List<double[]> extractVerticesFromOBJ(String filePath) {
+        List<double[]> vertices = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+
+            // Lee cada línea del archivo
+            while ((line = br.readLine()) != null) {
+                // Filtra las líneas que comienzan con 'v'
+                if (line.startsWith("v ")) {
+                    // Divide la línea en palabras y convierte las coordenadas a double
+                    String[] parts = line.split("\\s+");
+                    double x = Double.parseDouble(parts[1]);
+                    double y = Double.parseDouble(parts[2]);
+                    double z = Double.parseDouble(parts[3]);
+
+                    // Agrega las coordenadas a la lista de vértices
+                    vertices.add(new double[]{x, y, z});
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return vertices;
+    }
+
+    public void drawFigura(int posicionCubo, Color c) {
+        posicion = posicionCubo;
+        // rotate cube
+        for (int i = 0; i < vertices.size(); i++) {
+            xyRotate1(vertices.get(i), Math.sin(xyR), Math.cos(xyR));
+            xzRotate1(vertices.get(i), Math.sin(xzR), Math.cos(xzR));
+            yzRotate1(vertices.get(i), Math.sin(yzR), Math.cos(yzR));
+        }
+
+        // bordes
+        for (int i = 0; i < vertices.size(); i++) {
+            int j = (i + 1) % vertices.size();
+            drawEdge(vertices.get(i), vertices.get(j), c);
+        }
+    }
+
+    final void drawEdge(double[] p1, double[] p2, Color c) {
+        algoritmoDDALine((int) (p1[0] * scala) + posicion, (int) (-p1[1] * scala) + posicion,
+                (int) (p2[0] * scala) + posicion, (int) (-p2[1] * scala) + posicion, c);
+    }
+
 
 }
